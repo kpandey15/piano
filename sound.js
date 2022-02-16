acb7449d095cba6157d4937d4f95e106a062e91e
Kohina <pandeykohina@gmail.com>
2022-02-16 18:08:16 +0530

    first commit

---
+++ sound.js
@@ -0,0 +1,106 @@
+button = document.querySelectorAll('button');
+for(item of button){
+    item.addEventListener('click',(e)=>{
+        buttonText = e.target.innerText;
+        if(buttonText == 'F1'){
+            var audio = new Audio('key01.mp3');
+            audio.play();
+        }
+        if(buttonText == 'F1'){
+            var audio = new Audio('key01.mp3');
+            audio.play();
+        }
+        if(buttonText == 'G1'){
+            var audio = new Audio('key02.mp3');
+            audio.play();
+        }
+        if(buttonText == 'A1'){
+            var audio = new Audio('key03.mp3');
+            audio.play();
+        }
+        if(buttonText == 'B1'){
+            var audio = new Audio('key04.mp3');
+            audio.play();
+        }
+        if(buttonText == 'C1'){
+            var audio = new Audio('key05.mp3');
+            audio.play();
+        }
+        if(buttonText == 'D1'){
+            var audio = new Audio('key06.mp3');
+            audio.play();
+        }
+        if(buttonText == 'E1'){
+            var audio = new Audio('key07.mp3');
+            audio.play();
+        }
+        if(buttonText == 'F2'){
+            var audio = new Audio('key08.mp3');
+            audio.play();
+         }
+         if(buttonText == 'G2'){
+            var audio = new Audio('key09.mp3');
+            audio.play();
+         }
+         if(buttonText == 'A2'){
+            var audio = new Audio('key10.mp3');
+            audio.play();
+         }
+         if(buttonText == 'B2'){
+            var audio = new Audio('key11.mp3');
+            audio.play();
+         }
+         if(buttonText == 'C2'){
+            var audio = new Audio('key12.mp3');
+            audio.play();
+         }
+         if(buttonText == 'D2'){
+            var audio = new Audio('key13.mp3');
+            audio.play();
+         }
+         if(buttonText == 'E2'){
+            var audio = new Audio('key14.mp3');
+            audio.play();
+         }
+         if(buttonText == 'F3'){
+            var audio = new Audio('key15.mp3');
+            audio.play();
+         }
+         if(buttonText == 'G3'){
+            var audio = new Audio('key16.mp3');
+            audio.play();
+         }
+         if(buttonText == 'A3'){
+            var audio = new Audio('key17.mp3');
+            audio.play();
+         }
+         if(buttonText == 'B3'){
+            var audio = new Audio('key18.mp3');
+            audio.play();
+         }
+         if(buttonText == 'C3'){
+            var audio = new Audio('key19.mp3');
+            audio.play();
+         }
+         if(buttonText == 'D3'){
+            var audio = new Audio('key20.mp3');
+            audio.play();
+         }
+         if(buttonText == 'E3'){
+            var audio = new Audio('key21.mp3');
+            audio.play();
+         }
+         if(buttonText == 'F4'){
+            var audio = new Audio('key22.mp3');
+            audio.play();
+         }
+         if(buttonText == 'GA'){
+            var audio = new Audio('key23.mp3');
+            audio.play();
+         }
+         if(buttonText == 'A4'){
+            var audio = new Audio('key24.mp3');
+            audio.play();
+         }
+    })
+}
